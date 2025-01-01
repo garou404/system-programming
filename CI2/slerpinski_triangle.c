77cbf048c6cf298349cbf053a58eb2baaf4c68ba
garou404 <charlesmeyer347@gmail.com>
2025-01-01 17:36:59 +0100

    polygone creation added

--- CI2/slerpinski_triangle.c
+++ CI2/slerpinski_triangle.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct point {
+    float x;
+    float y;
+};
+
 int sort_get_index(float tab[], int top, float val) {
     for (int i = 0; i < top; i++) {
         if (val < tab[i]){
@@ -58,11 +63,47 @@ void plot_point(char grid[], int x, int y, char iv) {
     grid[nb_columns()*(nb_lines()-1-y) + x] = iv;
 }
 
+void plot_vline(char grid[], int x, float fy0, float fy1, char pixel) {
+    int y0 = (int) (fy0+0.5);
+    if ((y0-0.5) == fy0) {
+        y0 = y0 - 1;
+    }
+    int y1 = (int) (fy1+0.5);
+    for (int i = y0; i <= y1; i++) {
+        plot_point(grid, x, i, pixel);
+    }
+}
+
+void plot_poly_sweep(char grid[], struct point p[], int tab_length, int x, char pixel) {
+    printf("sweep: %d", x);
+    int j = tab_length - 1;
+    for (int i = 0; i < tab_length; i++) {
+        // printf(" (%d, %d)", j, i);
+        if (((p[i].x <= x) & (x <= p[j].x)) | ((p[j].x <= x) & (x <= p[i].x))) {
+            printf(" %f", p[i].y + (x - p[i].x) * (p[j].y - p[i].y) / (p[j].x - p[i].x));
+        }
+        j = i;
+    }
+    // for(int i = axis; i < (nb_columns()*nb_lines()); (i = i +nb_columns())) {
+    //     printf("%c", grid[i]);
+    // }
+    printf("\n");
+}
+
+void plot_poly(char grid[], struct point p[], int tab_length, char pixel) {
+    for (int o = 0; o < nb_columns(); o++) {
+        plot_poly_sweep(grid, p, tab_length, o, pixel);
+    }
+}
+
 int main(int argc, char** argv){
     char grid[nb_columns() * nb_lines()];
-    grid_init(grid, '*');
+    
+    grid_init(grid, ' ');
     plot_point(grid, 3, 1, ' ');
-    // plot_point(grid, 3, 100, ' ');
-    grid_display(grid);
+    int tab_length = 6;
+    struct point p[] = {{ 2, 13 }, { 10, 13 }, { 30, 7 }, { 10, 1 }, { 2, 1 }, { 18, 7 }};
+    plot_poly(grid, p, tab_length, '*');
+    // grid_display(grid);
     return EXIT_SUCCESS;
 }
