1bbbe259091d31c38617c36a69a26a58aaab2c46
garou404 <charlesmeyer347@gmail.com>
2025-02-14 12:40:02 +0100

    compilatiion and variables range section completed

---
+++ TP3/3-portee_variables/bar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int var_globale_extern;
+
+
+// int main(int argc,char**argv) {
+//     return EXIT_SUCCESS;
+// }
--- TP3/3-portee_variables/foo.c
+++ TP3/3-portee_variables/foo.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char**argv) {
+int var_globale = 0;
+static int var_globale_static = 0;
+static int var_globale_extern;
 
+int main(int argc, char**argv) {
+  int a1 = 0;
+  static int a2 = 0;
+
+  int result = var_globale + var_globale_static + var_globale_extern;
+  printf("%i\n", a1+a2);
   return EXIT_SUCCESS;
 }
