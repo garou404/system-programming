42bb4c70affb7a710c964b9dbd6fbc92f1ae003c
garou404 <charlesmeyer347@gmail.com>
2025-02-15 17:18:14 +0100

    makefile part completed

--- TP3/5-makefile/src/main.c
+++ TP3/5-makefile/src/main.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "foo.h"
+#include "../libtab/tab.h"
 
 int main(int argc, char**argv) {
   TYPE n = foo();
+  int tab_size = 10;
+  int tab[tab_size];
+  saisir_tab(tab, tab_size);  
   printf("%d\n", n);
   return EXIT_SUCCESS;
 }
